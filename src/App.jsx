7d0ea86d41004f7bd506364ac3dbd1a91d8f9318
makwanaom <117265350+makwanaom@users.noreply.github.com>
2024-04-10 10:57:23 +0530

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-
+import React from 'react';
 import Navbar from './components/navbar/Navbar'
 import Home from './page/home/Home'
 import Layer0 from './page/layer0/layer0'
+import Login from './components/auth/Login'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import { ThemeContextProvider } from './context/ThemeContext'
 import ThemeProvider from './providers/ThemeProvider'
@@ -10,11 +12,16 @@ const App = () => {
   return (
     <ThemeContextProvider>
       <ThemeProvider>
+
         <div className="container">
           <Navbar />
           <Home />
           <Layer0 />
+
+          
+
         </div>
+
       </ThemeProvider>
     </ThemeContextProvider>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+     
     </BrowserRouter>
   </React.StrictMode>
 );
